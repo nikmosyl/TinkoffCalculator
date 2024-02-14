f73423444c3207861d91a7d1a778fdc63454d806
2-39-swiftbook-joint-project <2.39.swiftbook.joint.project@gmail.com>
2024-02-14 17:31:05 +0400

    Initial Commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by nikita on 14.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
