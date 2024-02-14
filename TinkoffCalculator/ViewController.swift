3a3c7cf3e42a0920fee1782f0375b83f6d787cb9
2-39-swiftbook-joint-project <2.39.swiftbook.joint.project@gmail.com>
2024-02-14 18:58:39 +0400

    Buttons placed

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        print(buttonText)
+    }
 }
 
